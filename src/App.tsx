bfdf4927492e87109a8b85448b4bbea28437e73a
bdsdirect-sdm <bdsdirect.sdm@smartdatainc.net>
2024-10-14 07:28:14 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { Routes , Route} from 'react-router-dom';
+import Register from './components/Register';
+
+import UpdateForm from './components/UpdateForm';
+import Profile from './components/Profile';
+import Login from './components/Login';
+import AgencyDeshboard from './components/AgencyDashboard';
+import SeekerDashBoard from './components/SeekerDashboard';
+
+const App : React.FC = () => {
+  return (
+    <div className="App">
+      <Routes>
+        {/* <Route path='/' element = {<Home/>}/> */}
+        <Route path = "/" element = {<Register/>}/>
+        <Route path='/login' element ={<Login/>}/>
+        <Route path="/update/:id"  element = {< UpdateForm/>}/>
+        <Route path = "/profile/:id" element = {<Profile/>}/>
+        <Route path = "/dashboard" element = {
+          localStorage.getItem("user_type") === "Job_Seeker" ?(<SeekerDashBoard/>): (<AgencyDeshboard/>)
+        }/>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
